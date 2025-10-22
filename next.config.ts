20a1e22790225f7baac7d0f7448e62c158e03d09
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2025-10-22 11:59:55 +0530

    Update next.config.ts: Enable caching for components and React compiler, and add experimental support for Turbopack file system cache in development.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  cacheComponents: true,
+
+  reactCompiler: true,
+
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
